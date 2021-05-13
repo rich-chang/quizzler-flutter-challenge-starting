a7d66655812af42a006eb019751f56d62abba467
Rich <rich_chang@outlook.com>
2021-05-13 23:26:23 +0800

    Implement rfflutter_alert  challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
 QuizBrain quizBrain = QuizBrain();
@@ -36,24 +36,30 @@ class _QuizPageState extends State<QuizPage> {
 
     setState(() {
       //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
-      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
-      //HINT! Step 4 Part B is in the quiz_brain.dart
-      //TODO: Step 4 Part C - reset the questionNumber,
-      //TODO: Step 4 Part D - empty out the scoreKeeper.
-
-      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
-      if (userPickedAnswer == correctAnswer) {
-        scoreKeeper.add(Icon(
-          Icons.check,
-          color: Colors.green,
-        ));
+      if (quizBrain.isFinished()) {
+        //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+        Alert(context: context, title: "Notice", desc: "Questions is reaching to the last one. It will restart.").show();
+        //HINT! Step 4 Part B is in the quiz_brain.dart
+        quizBrain.printFinished();
+        //TODO: Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+        //TODO: Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
       } else {
-        scoreKeeper.add(Icon(
-          Icons.close,
-          color: Colors.red,
-        ));
+        //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
       }
-      quizBrain.nextQuestion();
     });
   }
 
